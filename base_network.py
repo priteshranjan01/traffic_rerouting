2941b150f580777ee7c60f553b451f456cb6e66d
Pritesh Ranjan <pranjan@ncsu.edu>
2017-04-23 23:19:17 -0400

    ARP broadcast working

--- base_network.py
+++ base_network.py
@@ -131,7 +131,7 @@ class BaseNetwork(app_manager.RyuApp):
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTION, actions)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
         mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match,
                                 instructions=inst)
         datapath.send_msg(mod)
